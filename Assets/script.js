2777ecc40951e02f4d7778205a2e1b0710440d2a
lnewmanheggie <lnewmanheggie@gmail.com>
2021-01-15 12:47:45 -0600

    created anther js page for saved events and orgainzed files

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,14 @@
-var city = "Saint Paul"; 
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=jAVZgW6que5FVdtiygGWfal7FvFxA8ue&city=" + city;
+// SAVED EVENTS BUTTON
+
+$("#saved-events").on("click", function() {
+    location.href = "saved-events.html";
+});
+
+
+// TICKETMASTER API CALL
+
+var queryUrlVenue = "https://app.ticketmaster.com/discovery/v2/venues.json?keyword=" + venueVal + "&apikey=jAVZgW6que5FVdtiygGWfal7FvFxA8ue"
+var queryURLCity = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=jAVZgW6que5FVdtiygGWfal7FvFxA8ue&city=" + cityVal;
 
 $.ajax({
 url: queryURL,
@@ -9,33 +18,33 @@ method: "GET"
     console.log(response)
     var results = response._embedded.events;
 
-    for (let i = 0; i < results.length; i++) {
+    for (var i = 0; i < results.length; i++) {
         if (results[i].classifications[0].segment.name === "Music") {
-            let name = results[i].name;
-            let ticketUrl = results[i].url;
-            let priceLow = "Unknown";
-            let priceHigh = "Unknown";
+            var name = results[i].name;
+            var ticketUrl = results[i].url;
+            var priceLow = "Unknown";
+            var priceHigh = "Unknown";
             if (results[i].priceRanges) {
                 priceLow = results[i].priceRanges[0].min
                 priceHigh = results[i].priceRanges[0].max
             }
-            let venue = results[i]._embedded.venues[0].name;
-            let lat = results[i]._embedded.venues[0].location.latitude;
-            let long = results[i]._embedded.venues[0].location.longitude;
+            var venue = results[i]._embedded.venues[0].name;
+            var lat = results[i]._embedded.venues[0].location.latitude;
+            var long = results[i]._embedded.venues[0].location.longitude;
             
-            let date = results[i].dates.start.localDate;
-            let time = results[i].dates.start.localTime;
+            var date = results[i].dates.start.localDate;
+            var time = results[i].dates.start.localTime;
             time = moment(time, "H").format("h A");
             date = moment(date, 'YYYY-MM-DD').format("l");
         }
     }
 });
 
-// Artist search button
+// Search button for artist, city, venue
 const searchBtn = $("#search")
-let artistVal;
-let cityVal;
-let venueVal;
+var artistVal;
+var cityVal;
+var venueVal;
 
 searchBtn.on("click", getValues)
 
