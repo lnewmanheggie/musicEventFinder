13ec719386ceea24a381a70611530a0cb540d91d
Samuel Nickson <samnickson@me.com>
2021-01-19 12:03:52 -0600

    saved events changes

--- Assets/savedEvents.js
+++ Assets/savedEvents.js
@@ -1,1 +1,46 @@
 // Put all the js for the saved events page in here
+function getSavedEvents() {
+    return JSON.parse(localStorage.getItem("savedEvents")) || []
+}
+
+createCards();
+
+
+function createCards(){
+    var getSaved = getSavedEvents();
+
+    var savedEventsBox = $("#saved-events-box");
+    savedEventsBox.empty();
+   
+    
+    var wrapper = $("<div>").addClass("notification is-primary");
+    savedEventsBox.append(wrapper);
+
+    for (let i = 0; i < getSaved.length; i++) {
+        console.log(getSaved[i])
+        var box = $("<div>").addClass("box mx-5 has-text-centered");
+        wrapper.append(box);
+        var content = $("<div>").addClass("content");
+        box.append(content);
+        content.append($("<h2>").text(getSaved[i].name))
+        content.append($("<p>").append($("<a>").attr("href", getSaved[i].url).text("Buy Tickets")));
+        
+        var columnBox = $("<div>").addClass("columns mx-4");
+        content.append(columnBox)
+        columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].city));
+        columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].venue));
+        columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].date));
+        columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].time));
+
+        var footer = $("<footer>").addClass("card-footer");
+        var saveBtn = $("<button>").addClass("button is-danger is-small is-fullwidth").text("Save").data("info", getSaved[i]);
+        footer.append(saveBtn);
+        content.append(footer);
+
+        // add event listner to grab the data when save btn is clicked
+        saveBtn.on("click", function() {
+            var savedData = $(this).data("info");
+            // put the data into local storage
+        })
+    }
+}
