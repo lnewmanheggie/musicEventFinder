d492f31b0b3bc266d30421e934657b0175c9c201
lnewmanheggie <lnewmanheggie@gmail.com>
2021-01-24 16:04:13 -0600

    cleaned up code

--- Assets/savedEvents.js
+++ Assets/savedEvents.js
@@ -1,53 +1,50 @@
 // Put all the js for the saved events page in here
-function getSavedEvents() {
-    return JSON.parse(localStorage.getItem("savedEvents")) || []
-}
+const getSavedEvents = () => JSON.parse(localStorage.getItem("savedEvents")) || [];
+const setSavedEvents = (val) => localStorage.setItem("savedEvents", JSON.stringify(val))
 
 createCards();
 
-
 function createCards(){
-    var getSaved = getSavedEvents();
+    const getSaved = getSavedEvents();
 
-    var savedEventsBox = $("#saved-events-box");
+    const savedEventsBox = $("#saved-events-box");
     savedEventsBox.empty();
-    var wrapper = $("<div>").addClass("notification is-primary");
+    const wrapper = $("<div>").addClass("notification is-primary");
     savedEventsBox.append(wrapper);
 
     if (getSaved.length > 0) {
         for (let i = 0; i < getSaved.length; i++) {
             
-            var box = $("<div>").addClass("box mx-5 has-text-centered");
+            const box = $("<div>").addClass("box mx-5 has-text-centered");
             wrapper.append(box);
-            var content = $("<div>").addClass("content");
+            const content = $("<div>").addClass("content");
             box.append(content);
             content.append($("<h2>").text(getSaved[i].name))
             content.append($("<p>").append($("<a>").attr("href", getSaved[i].url).text("Buy Tickets")));
             
-            var columnBox = $("<div>").addClass("columns mx-4");
+            const columnBox = $("<div>").addClass("columns mx-4");
             content.append(columnBox)
             columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].city));
             columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].venue));
             columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].date));
             columnBox.append($("<div>").addClass("column is-size-5").text(getSaved[i].time));
 
-            var footer = $("<footer>").addClass("card-footer");
-            var delBtn = $("<button>").addClass("button is-info is-small is-fullwidth").text("Delete").data("info", getSaved[i]);
+            const footer = $("<footer>").addClass("card-footer");
+            const delBtn = $("<button>").addClass("button is-info is-small is-fullwidth").text("Delete").data("info", getSaved[i]);
             footer.append(delBtn);
             content.append(footer);
 
             // delete button 
             delBtn.on("click", function() {
-                
-
-                var delData = $(this).data("info");
-                var index = getSaved.indexOf(delData)
+                const delData = $(this).data("info");
+                const index = getSaved.indexOf(delData)
                 getSaved.splice(index, 1);
 
                 setSavedEvents(getSaved);
 
-                var deleteCard = $(this).parent().parent().parent();
+                const deleteCard = $(this).parent().parent().parent();
                 deleteCard.remove();
+                wrapper.append($("<h3>").addClass("has-text-centered is-size-5").text("No saved events"))
             })
         }
     } else {
@@ -55,6 +52,4 @@ function createCards(){
     }
 }
 
-function setSavedEvents(val) {
-    localStorage.setItem("savedEvents", JSON.stringify(val))
-}
+
--- Assets/script.js
+++ Assets/script.js
@@ -5,12 +5,12 @@ $("#saved-events").on("click", function () {
 });
 
 // virtual event toggler
-$("li").on("click",function(){
-    var listItem =  $(this).attr("class");
-    if(listItem === 'is-active'){
+$("li").on("click", function () {
+    const listItem = $(this).attr("class");
+    if (listItem === 'is-active') {
         $(this).removeAttr("class");
         $(this).siblings().addClass("is-active")
-    }else{
+    } else {
         $(this).addClass("is-active")
         $(this).siblings().removeAttr("class")
     }
@@ -18,63 +18,61 @@ $("li").on("click",function(){
 
 // start of location code
 // start of location pull 
-var locationofuser = $("#events-near-me");
-var latInput;
-var longInput;
+const locationofuser = $("#events-near-me");
+let latInput;
+let longInput;
 
 locationofuser.on("click", getLocation);
 
 function getLocation() {
-    
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
-    } else { 
+    } else {
         locationofuser.text("Geolocation is not supported by this browser.");
     }
 }
-    
+
 function showPosition(position) {
     // locationofuser.text("Latitude: " + position.coords.latitude + 
     // "<br>Longitude: " + position.coords.longitude)
-    latInput = position.coords.latitude ;
+    latInput = position.coords.latitude;
     longInput = position.coords.longitude;
     locationAPIconvert();
-// start of location conversion
-    function locationAPIconvert() {
-        
-        var queryURL = "https://us1.locationiq.com/v1/reverse.php?key=pk.ecdb52a710229c9ea2d12411c0c751c6&lat="+ latInput +"&lon="+ longInput +"&format=json"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var cityofuser = response.address.city
+    // start of location conversion
 
-                $("#city").val(cityofuser);
-                getValues();
-            });
-    }
 }
+function locationAPIconvert() {
+    const queryURL = "https://us1.locationiq.com/v1/reverse.php?key=pk.ecdb52a710229c9ea2d12411c0c751c6&lat=" + latInput + "&lon=" + longInput + "&format=json"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var cityofuser = response.address.city
 
+            $("#city").val(cityofuser);
+            getValues();
+        });
+}
 // end of location pull & conversion
 
 
 // Regular search function
-var searchBtn = $("#search");
-var searchContainer = $("#search-container");
-var error = $("<p>").attr("id", "error-message").addClass("has-text-centered");
+const searchBtn = $("#search");
+const searchContainer = $("#search-container");
+const error = $("<p>").attr("id", "error-message").addClass("has-text-centered");
 
 searchBtn.on("click", getValues);
 
 function getValues() {
     // check if item is virtual and if so, populate venue value with "live stream"
-    var virtual = $("#virtual").attr("class")
+    const virtual = $("#virtual").attr("class")
     if (virtual === "is-active") {
         $("#venue").val("live stream");
-    } 
-        
-    
-    var search = {  
+    }
+
+
+    const search = {
         artist: $("#artist").val() || false,
         city: $("#city").val() || false,
         venue: $("#venue").val() || false
@@ -84,23 +82,23 @@ function getValues() {
         error.text("You may search by artist, city, or venue, or by artist + one other value");
         searchContainer.append(error)
         return;
-    }    
+    }
 
     if (search.artist) {
-       return bandsintownApiCall(search.artist, search.city, search.venue)
+        return bandsintownApiCall(search.artist, search.city, search.venue)
     }
-    ticketmasterApiCall(search.artist, search.city, search.venue)
 
+    ticketmasterApiCall(search.artist, search.city, search.venue)
 }
 
-function bandsintownApiCall(artist, city, venue) {
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=test&date=upcoming";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function (response) {
-        var events = response.map(event => ({
+async function bandsintownApiCall(artist, city, venue) {
+    const queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=test&date=upcoming";
+    try {
+        const response = await $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        const events = response.map(event => ({
             name: event.lineup.join(", "),
             url: event.url,
             venue: event.venue.name,
@@ -109,29 +107,35 @@ function bandsintownApiCall(artist, city, venue) {
             date: moment(event.datetime).format("l"),
         }))
         filterEvents(events, artist, city, venue)
-    });
+    } catch (error) {
+        console.warn(error)
+    }
 }
 
 function ticketmasterApiCall(artist, city, venue) {
-    var term = city || venue
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + term + "&apikey=jAVZgW6que5FVdtiygGWfal7FvFxA8ue"
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            var events = response._embedded.events
-                .filter(event => event.classifications[0].segment.name === "Music")
-                .map(event => ({
-                    name: event.name,
-                    url: event.url,
-                    venue: event._embedded.venues[0].name,
-                    city: event._embedded.venues[0].city.name,
-                    time: moment(event.dates.start.localTime, "HH:mm").format("h:mm A"),
-                    date: moment(event.dates.start.localDate, "YYYY-MM-DD").format("l"),
-                }))
-            filterEvents(events, artist, city, venue)
-        });
+    const term = city || venue
+    const queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + term + "&apikey=jAVZgW6que5FVdtiygGWfal7FvFxA8ue"
+    try {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                const events = response._embedded.events
+                    .filter(event => event.classifications[0].segment.name === "Music")
+                    .map(event => ({
+                        name: event.name,
+                        url: event.url,
+                        venue: event._embedded.venues[0].name,
+                        city: event._embedded.venues[0].city.name,
+                        time: moment(event.dates.start.localTime, "HH:mm").format("h:mm A"),
+                        date: moment(event.dates.start.localDate, "YYYY-MM-DD").format("l"),
+                    }))
+                filterEvents(events, artist, city, venue)
+            });
+    } catch(error) {
+        console.warn(error)
+    }
 }
 
 // THIS FUNCTION RETURNS RELEVANT DATA
@@ -148,67 +152,57 @@ function filterEvents(events, artist, city, venue) {
 
 // CREATE THE CARDS AND POPULATE WITH RESULTS
 
-var resultsText = $("#results-text");
-var resultsBox = $("#results-box");
+const resultsText = $("#results-text");
+const resultsBox = $("#results-box");
 
-function createCards (filtered){
+function createCards(filtered) {
     resultsText.empty();
     resultsBox.empty();
     // have a condition here for the number of results
-    resultsText.append($("<h2>").addClass("is-size-3").text("Results"))
-    var wrapper = $("<div>").addClass("notification is-primary");
+    resultsText.append($("<h2 class='is-size-3'>Results</h2>"))
+
+    const wrapper = $("<div>").addClass("notification is-primary");
     resultsBox.append(wrapper);
 
     for (let i = 0; i < filtered.length; i++) {
-        var box = $("<div>").addClass("box mx-5 has-text-centered");
+        const box = $("<div>").addClass("box mx-5 has-text-centered");
         wrapper.append(box);
-        var content = $("<div>").addClass("content");
+        const content = $("<div>").addClass("content");
         box.append(content);
         content.append($("<h2>").text(filtered[i].name))
-        content.append($("<p>").append($("<a>").attr("href", filtered[i].url).text("Buy Tickets")));
-        
-        var columnBox = $("<div>").addClass("columns mx-4");
+        content.append($("<p>").append($("<a>").attr("href", filtered[i].url).attr("target", "_blank").text("Buy Tickets")));
+
+        const columnBox = $("<div>").addClass("columns mx-4");
         content.append(columnBox)
         columnBox.append($("<div>").addClass("column is-size-5").text(filtered[i].city));
         columnBox.append($("<div>").addClass("column is-size-5").text(filtered[i].venue));
         columnBox.append($("<div>").addClass("column is-size-5").text(filtered[i].date));
         columnBox.append($("<div>").addClass("column is-size-5").text(filtered[i].time));
 
-        var footer = $("<footer>").addClass("card-footer");
-        var saveBtn = $("<button>").addClass("button is-danger is-small is-fullwidth").text("Save").data("info", filtered[i]);
+        const footer = $("<footer>").addClass("card-footer");
+        const saveBtn = $("<button>").addClass("button is-danger is-small is-fullwidth").text("Save").data("info", filtered[i]);
         footer.append(saveBtn);
         content.append(footer);
 
         // add event listner to grab the data when save btn is clicked
         saveBtn.on("click", saveDataLocalStorage);
-        
-        
-        function saveDataLocalStorage() {
-            var savedData = $(this).data("info");
-
-            var savedEvents = getSavedEvents();
-
-            savedEvents.push(savedData);
-
-            setSavedEvents(savedEvents);
-            
-        }
     }
 }
 
+const getSavedEvents = () => JSON.parse(localStorage.getItem("savedEvents")) || []
+
+const setSavedEvents = (val) => localStorage.setItem("savedEvents", JSON.stringify(val))
+
+function saveDataLocalStorage() {
+    const savedData = $(this).data("info");
+
+    const savedEvents = getSavedEvents();
+
+    savedEvents.push(savedData);
+
+    setSavedEvents(savedEvents);
+
+}
 
 // FUNCTIONS TO SAVE EVENTS INTO LOCAL STORAGE AND GET EVENTS OUT OF LOCAL STORAGE
-
-function getSavedEvents() {
-    return JSON.parse(localStorage.getItem("savedEvents")) || []
-}
-
-function setSavedEvents(val) {
-    localStorage.setItem("savedEvents", JSON.stringify(val))
-}
-
-
-
-
-
 
